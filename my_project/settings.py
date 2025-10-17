95c5a4b48ae9a6fdc615289d1e8a816fce9202e3
AlexandraSelby <dagnaa19@gmail.com>
2025-10-17 14:56:18 +0100

    feat: wire reviews app with home route

--- my_project/settings.py
+++ my_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reviews'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'my_project.urls'
+ROOT_URLCONF = "my_project.urls"
 
 TEMPLATES = [
     {
