2963ccd69a3fd4809fd6f16b524d8cf4b2b73cd5
AlexandraSelby <dagnaa19@gmail.com>
2025-11-13 20:11:14 +0000

    feat: connect to Heroku PostgreSQL database

--- my_project/settings.py
+++ my_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 from dotenv import load_dotenv
+import dj_database_url
 import os
 
 load_dotenv()
@@ -88,10 +89,10 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',   # fallback for local dev
+        conn_max_age=600,
+    )
 }
 
 # Password validation
